8c60258732f0db163d2b232d3dc81c6d350f25b2
Olga Bogdanova <o.bahdanavaa@gmail.com>
2023-01-12 20:18:43 +0300

    switch...case

---
+++ Task 4.js
@@ -0,0 +1,41 @@
+let defineSeasonAndQueueOfMonth = 'August'; 
+switch (defineSeasonAndQueueOfMonth) {
+case 'January' :
+    console.log( 'Second winter month' );
+    break;
+case 'February' :
+    console.log( 'Third winter month' );
+    break;
+case 'March':
+    console.log( 'First spring month' );
+    break;
+case 'April':
+    console.log( 'Second spring month' );
+    break;
+case 'May':
+    console.log( 'Third spring month' );
+    break;
+case 'June':
+    console.log( 'First summer month' );
+    break;
+case 'July':
+    console.log( 'Second summer month' );
+    break; 
+case 'August':
+    console.log( 'Third summer month' );
+    break;
+case 'September':
+    console.log( 'First autumn month' );
+    break;
+case 'October':
+    console.log( 'Second autumn month' );
+    break;
+case 'November':
+    console.log( 'Third autumn month' );
+    break;
+case 'December':
+    console.log( 'Third winter month' );
+    break;
+default:
+    console.log('Value not found. Make sure that the data is entered correctly') 
+}
