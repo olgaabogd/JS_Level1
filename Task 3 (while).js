b5436da55c53bd924c55bf06ca0033655b73119e
Olga Bogdanova <o.bahdanavaa@gmail.com>
2023-01-12 20:09:23 +0300

    3 (while)

---
+++ Task 3 (while).js
@@ -0,0 +1,12 @@
+let countBonusSystem = [5, 10, 2, 52, 2, 3, 5, 5, 6, 89]; 
+let i = 0; 
+
+console.log ('Sum for winners (х2):')
+while (i <= 2) {
+console.log( countBonusSystem[i] * 2 ); 
+i++ } 
+
+console.log ('Sum for participants:')
+while (i < countBonusSystem.length ) {
+console.log(countBonusSystem[i]); 
+i++} 
