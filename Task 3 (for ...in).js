322b0a0f5aa0f4b98ebce003827d30aba495b87e
Olga Bogdanova <o.bahdanavaa@gmail.com>
2023-01-12 20:10:59 +0300

    3 (for...in)

---
+++ Task 3 (for ...in).js
@@ -0,0 +1,16 @@
+let informationAboutMe = {
+    name: 'Olga',
+    company: 'iTechArt',
+    position: 'QA engineer'
+}
+
+for ( let infBlock in informationAboutMe ) {
+console.log(infBlock, ':', informationAboutMe[infBlock])
+}
+
+for ( let infBlock in informationAboutMe ) {
+console.log(infBlock) 
+}
+
+for ( let infBlock in informationAboutMe ) {
+console.log(informationAboutMe[infBlock])} 
